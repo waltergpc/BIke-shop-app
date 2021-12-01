1a35d92888492fea520d3f7e6bca45055f92c753
Walter González-Plata Cortes <waltergpc@hotmail.com>
2021-11-30 23:43:30 -0600

    removed .env from push

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY)
 
 exports.handler = async function (event, context) {
   if (event.body) {
-    const { cart, shippingFee, totalAmount } = JSON.parse(event.body)
+    const { shippingFee, totalAmount } = JSON.parse(event.body)
     const calculateOrderAmount = () => {
       return shippingFee + totalAmount
     }
